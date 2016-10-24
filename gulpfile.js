e192dbec4d27a0ecd660860fbe6227beefce25cc
Sachin <sachinbapure@gmail.com>
2016-10-24 19:35:08 +0530

    ShareWay Starts

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+
+
+
+
+gulp.task('browserSync',function () {
+  browserSync.init({
+    server:{
+      baseDir : 'app'
+    }
+  })
+})
+
+
+gulp.task('sass',function () {
+  return gulp.src('app/scss/**/*.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('app/css'))
+  .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/js/**/*.js',browserSync.reload)
+  gulp.watch('app/*.html',browserSync.reload)
+  // Other watchers
+});
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
